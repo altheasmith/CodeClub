481cfdf53c7512af1ec9c2cb0ae72d156bb0abbc
Althea Smith <asmith@cpxi.com>
2017-03-10 14:57:54 -0500

    #songsincode

---
+++ songsincode/song.js
@@ -0,0 +1,16 @@
+var actions = ['give you up',
+              'let you down',
+              'run around and desert you',
+              'make you cry',
+              'say goodbye',
+              'tell a lie and hurt you'];
+
+if (0 > 1) {
+  for (var i = 0; i < actions.length; i++) {
+    console.log("I will", actions[i]);
+  }
+} else {
+  for (var i = 0; i < actions.length; i++) {
+    console.log("Never gonna", actions[i]);
+  }
+}
---
+++ songsincode/song.py
@@ -0,0 +1,13 @@
+actions = ['give you up',
+              'let you down',
+              'run around and desert you',
+              'make you cry',
+              'say goodbye',
+              'tell a lie and hurt you'];
+
+if 0 > 1:
+  for action in actions:
+     print "I will", action
+else:
+  for action in actions:
+    print "Never gonna", action
